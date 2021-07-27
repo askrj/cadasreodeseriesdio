e084ee4a3a7a4b6d0bef890a5757068d5132d5d3
Alex Santos <ask.rj10@gmail.com>
2021-07-27 20:24:05 -0300

    projeto dio

---
+++ CadSerie/Class/EntidadeBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadSerie.Class
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
